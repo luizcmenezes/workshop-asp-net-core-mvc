2d3f909d0652e618c0bc7ac4693d909a212de926
luizcmenezes <lcmscontas@gmail.com>
2020-03-20 09:53:24 -0300

    Foreign key not null

--- SalesWebMvc/Services/SellerServices.cs
+++ SalesWebMvc/Services/SellerServices.cs
@@ -22,6 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _conetxt.Departments.First();
             _conetxt.Add(obj);
             _conetxt.SaveChanges();
         }
